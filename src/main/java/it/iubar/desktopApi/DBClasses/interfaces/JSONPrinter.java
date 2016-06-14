9a3489f61ff75eca85cd134dca0e690da3d3fb16
Tbaile <bailetti.tommaso@gmail.com>
2016-06-14 17:17:41 +0200

    removed interface, added abstract class

--- src/main/java/it/iubar/desktopApi/DBClasses/interfaces/JSONPrinter.java
+++
@@ -1,7 +0,0 @@
-package it.iubar.desktopApi.DBClasses.interfaces;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface JSONPrinter {
-    public String toJson() throws JsonProcessingException;
-}
