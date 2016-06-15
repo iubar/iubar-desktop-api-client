1b3ad8cf8152a112c2f2a4ffd1c328e242b9e244
Tbaile <bailetti.tommaso@gmail.com>
2016-06-15 10:59:27 +0200

    removed Cloner

--- src/main/java/it/iubar/desktopApi/DBClasses/JSONPrinter.java
+++ src/main/java/it/iubar/desktopApi/DBClasses/JSONPrinter.java
@@ -3,15 +3,13 @@ package it.iubar.desktopApi.DBClasses;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.rits.cloning.Cloner;
 
 public class JSONPrinter {
 
     public static <T> String toJson(T obj) throws JsonProcessingException, CloneNotSupportedException {
         ObjectMapper mapper = new ObjectMapper();
-        Cloner cloner = new Cloner();
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cloner.deepClone(obj));
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
     }
 
 }
