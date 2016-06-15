ab97599fe6e27d1953f30802abeba4287ef56eaa
Tbaile <bailetti.tommaso@gmail.com>
2016-06-15 09:55:24 +0200

    added Datore Class

---
+++ src/main/java/it/iubar/desktopApi/DBClasses/Datore.java
@@ -0,0 +1,113 @@
+package it.iubar.desktopApi.DBClasses;
+
+public class Datore extends JSONPrinter {
+    private int iddatore;
+    private String cf;
+    private String piva;
+    private String denom;
+    private String sub;
+    private String para;
+    private String email;
+    private String tel;
+    private int idcomune;
+    private int idprovincia;
+
+    public Datore(int iddatore, String cf, String piva, String denom, String sub, String para, String email, String tel, int idcomune, int idprovincia) {
+        this.setIddatore(iddatore);
+        this.setCf(cf);
+        this.setPiva(piva);
+        this.setDenom(denom);
+        this.setSub(sub);
+        this.setPara(para);
+        this.setEmail(email);
+        this.setTel(tel);
+        this.setIdcomune(idcomune);
+        this.setIdprovincia(idprovincia);
+    }
+
+    public int getIddatore() {
+        return iddatore;
+    }
+
+    public void setIddatore(int iddatore) {
+        this.iddatore = iddatore;
+    }
+
+    public String getCf() {
+        return cf;
+    }
+
+    public void setCf(String cf) {
+        this.cf = cf;
+    }
+
+    public String getPiva() {
+        return piva;
+    }
+
+    public void setPiva(String piva) {
+        this.piva = piva;
+    }
+
+    public String getDenom() {
+        return denom;
+    }
+
+    public void setDenom(String denom) {
+        this.denom = denom;
+    }
+
+    public String getSub() {
+        return sub;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public String getPara() {
+        return para;
+    }
+
+    public void setPara(String para) {
+        this.para = para;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getIdcomune() {
+        return idcomune;
+    }
+
+    public void setIdcomune(int idcomune) {
+        this.idcomune = idcomune;
+    }
+
+    public int getIdprovincia() {
+        return idprovincia;
+    }
+
+    public void setIdprovincia(int idprovincia) {
+        this.idprovincia = idprovincia;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Datore datore = new Datore(getIddatore(), getCf(), getPiva(), getDenom(), getSub(), getPara(), getEmail(), getTel(), getIdcomune(), getIdprovincia());
+        return datore;
+    }
+}
---
+++ src/test/java/it/iubar/desktopApi/DBClasses/DatoreTest.java
@@ -0,0 +1,34 @@
+package it.iubar.desktopApi.DBClasses;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatoreTest {
+
+    Datore datore;
+
+    @Before
+    public void before(){
+        datore = new Datore(2, "AIHDUAWHDOU", "obaojwdboawdb", "AWDBAWJDBJLA", "ADWADAWD", "EBOLA", "awdnaowdj@gmail.com", "0129300823", 2, 4);
+    }
+
+    @Test
+    public void testToJson() throws Exception{
+        String string = "{\n" +
+                "  \"iddatore\" : 2,\n" +
+                "  \"cf\" : \"AIHDUAWHDOU\",\n" +
+                "  \"piva\" : \"obaojwdboawdb\",\n" +
+                "  \"denom\" : \"AWDBAWJDBJLA\",\n" +
+                "  \"sub\" : \"ADWADAWD\",\n" +
+                "  \"para\" : \"EBOLA\",\n" +
+                "  \"email\" : \"awdnaowdj@gmail.com\",\n" +
+                "  \"tel\" : \"0129300823\",\n" +
+                "  \"idcomune\" : 2,\n" +
+                "  \"idprovincia\" : 4\n" +
+                "}";
+        assertEquals(string, datore.toJson());
+    }
+
+}
