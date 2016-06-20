410a50ca0a7da75001aacb776f475e23e86f5d88
Tbaile <bailetti.tommaso@gmail.com>
2016-06-20 16:37:26 +0200

    Changelog
    Now controls if the ini file is correct
    Implemented the `send()` method

--- src/main/java/it/iubar/desktop/api/MasterClient.java
+++ src/main/java/it/iubar/desktop/api/MasterClient.java
@@ -1,5 +1,17 @@
 package it.iubar.desktop.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import it.iubar.desktop.api.exceptions.ClientException;
+import it.iubar.desktop.api.models.Ccnl;
+import it.iubar.desktop.api.models.Datore;
+import it.iubar.desktop.api.models.Titolare;
+import it.iubar.desktop.api.services.JSONPrinter;
+import org.json.JSONObject;
+
+import javax.ws.rs.core.MediaType;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,9 +88,90 @@ public class MasterClient {
         Properties properties = new Properties();
         InputStream inputStream = new FileInputStream(this.getConfigDir());
         properties.load(inputStream);
-        return "http://" + properties.getProperty(HOST_VALUE) + ":" + properties.getProperty(PORT_VALUE, "80") + properties.getProperty(PATH_VALUE, "/");
+
+        String host = properties.getProperty(HOST_VALUE);
+        if(host == null){
+            LOGGER.log(Level.SEVERE, "Could not find 'host' in ini file.");
+            throw new RuntimeException("Could not find 'host' in ini file.");
+        }else{
+            host = normalizeHost(host);
+        }
+
+        String port = properties.getProperty(PORT_VALUE, "80");
+        port = normalizePort(port);
+
+        String path = properties.getProperty(PATH_VALUE, "");
+        path = normalizePath(path);
+
+        return "http://" + host + ":" + port  + path ;
     }
 
+    private String normalizePath(String s) {
+        if(!s.equalsIgnoreCase(""))
+            if(!s.substring(0,1).equalsIgnoreCase("/"))
+                s = "/" + s;
 
+        return s;
+    }
+
+    private String normalizeHost(String s){
+        if(s.contains("http://"))
+            s = s.replace("http://", "");
+        if(s.contains("https://"))
+            s = s.replace("https://", "");
+        if(s.contains(":"))
+            s = s.substring(0,s.indexOf(":"));
+
+        return s;
+    }
+
+    private String normalizePort(String s){
+        if(!s.equalsIgnoreCase("80"))
+            if(s.contains(":"))
+                s = s.replace(":", "");
+        return s;
+    }
+
+    public <T> void send(T obj) throws ClientException {
+
+        //Initialization jersey client
+        Client client = Client.create();
+        //set destination url
+        WebResource webResource = null;
+
+        //FIXME : remove manual data
+        if(obj instanceof it.iubar.desktop.api.models.Client)
+            url += "/insertClient";
+        else if (obj instanceof Datore)
+            url += "/insertDatore";
+        else if(obj instanceof Titolare)
+            url += "/insertTitolare";
+        else if(obj instanceof Ccnl)
+            url += "/insertCcnl";
+
+        webResource = client.resource(url);
+
+        String toJson = null;
+
+        try {
+            toJson = JSONPrinter.toJson(obj);
+        } catch (JsonProcessingException e) {
+            LOGGER.log(Level.SEVERE, "Jackson could not convert correctly", JsonProcessingException.class);
+            throw new RuntimeException("Jackson ha problemi a convertire la classe.");
+        }
+
+        //execution query
+        ClientResponse response = webResource.accept("application/json").type(MediaType.APPLICATION_JSON_TYPE).header("X-Requested-With", "XMLHttpRequest").post(ClientResponse.class, toJson);
+
+        JSONObject jsonResponse = new JSONObject(response.getEntity(String.class));
+        System.out.println(jsonResponse.toString());
+
+        //if code is not 200, throw exception
+        int stat = response.getStatus();
+        if (stat != 200) {
+            LOGGER.log(Level.SEVERE, "Error found in response: CODE " + stat);
+            throw new ClientException(response.getStatus(), jsonResponse.get("response").toString());
+        }
+    }
 
 }
