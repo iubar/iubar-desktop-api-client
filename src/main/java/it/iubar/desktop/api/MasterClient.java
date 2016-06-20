25db2ee07b050ecdec60b3759aee28e7e1218cac
Tbaile <bailetti.tommaso@gmail.com>
2016-06-20 17:27:20 +0200

    added "isUnique" value
    removed unused print at the bottom

--- src/main/java/it/iubar/desktop/api/MasterClient.java
+++ src/main/java/it/iubar/desktop/api/MasterClient.java
@@ -27,6 +27,7 @@ public class MasterClient {
     private final String HOST_VALUE = "host";
     private final String PORT_VALUE = "port";
     private final String PATH_VALUE = "path";
+    private final String IS_UNIQUE_VALUE = "is_unique";
 
     private final String INSERT_CLIENT = "/insertClient";
     private final String INSERT_TITOLARE = "/insertTitolare";
@@ -38,17 +39,20 @@ public class MasterClient {
     private boolean isAuth;
 
     private String url;
+    private boolean unique_url;
 
     public MasterClient(String configDir) throws IOException {
         this.setAuthValue(IS_AUTH_VALUE);
         this.setConfigDir(configDir);
         this.url = getUrl();
+        this.unique_url = isUnique();
     }
 
     public MasterClient(String configDir, String isAuthValue) throws IOException {
         this.setAuthValue(isAuthValue);
         this.setConfigDir(configDir);
         this.url = getUrl();
+        this.unique_url = isUnique();
     }
 
     public void setAuthValue(String authValue) {
@@ -75,18 +79,39 @@ public class MasterClient {
         Properties prop = new Properties();
         InputStream input = new FileInputStream(this.getConfigDir());
         prop.load(input);
-        String result = prop.getProperty(this.getAuthValue(), "false");
+        String result = prop.getProperty(IS_AUTH_VALUE, "false");
         if(result.equalsIgnoreCase("true"))
             isAuth = true;
         else
             isAuth = false;
-        LOGGER.log(Level.FINE, "Output given: " + result);
+        LOGGER.log(Level.FINE, "Auth Factor : " + result);
         if (input != null) {
             input.close();
             LOGGER.log(Level.FINE, "File closed.");
         }
 
         return isAuth;
+    }
+
+    private boolean isUnique() throws IOException {
+
+        boolean isUnique;
+
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream(this.getConfigDir());
+        prop.load(input);
+        String result = prop.getProperty(IS_UNIQUE_VALUE, "false");
+        if(result.equalsIgnoreCase("true"))
+            isUnique = true;
+        else
+            isUnique = false;
+        LOGGER.log(Level.FINE, "Single URL : " + result);
+        if (input != null) {
+            input.close();
+            LOGGER.log(Level.FINE, "File closed.");
+        }
+
+        return isUnique;
     }
 
     private String getUrl() throws IOException {
@@ -143,15 +168,17 @@ public class MasterClient {
         Client client = Client.create();
         //set destination url
         WebResource webResource = null;
-        
-        if(obj instanceof it.iubar.desktop.api.models.Client)
-            url += INSERT_CLIENT;
-        else if (obj instanceof Datore)
-            url += INSERT_DATORE;
-        else if(obj instanceof Titolare)
-            url += INSERT_TITOLARE;
-        else if(obj instanceof Ccnl)
-            url += INSERT_CCNL;
+
+        if (!unique_url) {
+            if(obj instanceof it.iubar.desktop.api.models.Client)
+                url += INSERT_CLIENT;
+            else if (obj instanceof Datore)
+                url += INSERT_DATORE;
+            else if(obj instanceof Titolare)
+                url += INSERT_TITOLARE;
+            else if(obj instanceof Ccnl)
+                url += INSERT_CCNL;
+        }
 
         webResource = client.resource(url);
 
@@ -168,7 +195,6 @@ public class MasterClient {
         ClientResponse response = webResource.accept("application/json").type(MediaType.APPLICATION_JSON_TYPE).header("X-Requested-With", "XMLHttpRequest").post(ClientResponse.class, toJson);
 
         JSONObject jsonResponse = new JSONObject(response.getEntity(String.class));
-        System.out.println(jsonResponse.toString());
 
         //if code is not 200, throw exception
         int stat = response.getStatus();
