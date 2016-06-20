4baf08c5800bc6394086eb17a8a5680ee34a7f1d
Tbaile <bailetti.tommaso@gmail.com>
2016-06-20 12:17:25 +0200

    Removed prettyPrinter
    The escape characters will break server-side

--- src/main/java/it/iubar/desktop/api/services/JSONPrinter.java
+++ src/main/java/it/iubar/desktop/api/services/JSONPrinter.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.Writer;
+
 public class JSONPrinter {
 
-    public static <T> String toJson(T obj) throws JsonProcessingException, CloneNotSupportedException {
+    public static <T> String toJson(T obj) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        return mapper.writeValueAsString(obj);
     }
 
 }
