54a2a3d0b794015200714db7144b43f53ff1749e
borgogelli <borgogelli@iubar.it>
2018-02-21 09:33:15 +0100

    Committed by Robo (from Borgo-PC at 2018-02-21 09:32) on 2018-02-21 09:32

--- src/main/java/it/iubar/desktop/api/models/RootModel.java
+++ src/main/java/it/iubar/desktop/api/models/RootModel.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -29,6 +30,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 
+import org.json.JSONObject;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
@@ -44,125 +46,108 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 public class RootModel {
-    
+
 	private final static Logger LOGGER = Logger.getLogger(RootModel.class.getName());
 	private final static DateFormat FORMAT1 = new SimpleDateFormat("yyyy-MM-dd");
 	private final static DateFormat FORMAT2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private final static DateFormat FORMAT3 = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
- 
-	
+
 	public String asXml() throws JAXBException {
 		Class c = this.getClass();
 		JAXBContext jContext = JAXBContext.newInstance(c);
-		java.io.StringWriter sw = new StringWriter();			
+		java.io.StringWriter sw = new StringWriter();
 		Marshaller marshaller = jContext.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		marshaller.marshal(this, sw);			
+		marshaller.marshal(this, sw);
 		return sw.toString();
 	}
-	
-    public static <T> T fromXml(Source xmlSource, Class<T> c) {
-    	T t = null;
+
+	public static <T> T fromXml(Source xmlSource, Class<T> c) {
+		T t = null;
 		try {
 			JAXBContext jc = JAXBContext.newInstance(c);
-	        Unmarshaller unmarshaller = jc.createUnmarshaller();
-	        t = (T) unmarshaller.unmarshal(xmlSource);				
-		} catch (JAXBException e) {				
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			t = (T) unmarshaller.unmarshal(xmlSource);
+		} catch (JAXBException e) {
 			e.printStackTrace();
-		}	 
-        return t;
-    }
-    
-    public static <T> T fromXml(Reader reader, Class<T> c) {
-    	T t = null;
+		}
+		return t;
+	}
+
+	public static <T> T fromXml(Reader reader, Class<T> c) {
+		T t = null;
 		try {
 			JAXBContext jc = JAXBContext.newInstance(c);
-	        Unmarshaller unmarshaller = jc.createUnmarshaller();
-	        t = (T) unmarshaller.unmarshal(reader);				
-		} catch (JAXBException e) {				
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			t = (T) unmarshaller.unmarshal(reader);
+		} catch (JAXBException e) {
 			e.printStackTrace();
-		}	 
-        return t;
-    }
-    
-    public static <T> T fromXml(String xmlString, Class<T> c) {
-    	DOMSource xmlSource = toDOMSource(xmlString);
-        return fromXml(xmlSource, c);
-    }
-    
-    public static <T> T fromXml2(String xmlString, Class<T> c) {
-    	Reader reader = new StringReader(xmlString);
-        return fromXml(reader, c);
-    }
-    
-	public String asJson(){
+		}
+		return t;
+	}
+
+	public static <T> T fromXml(String xmlString, Class<T> c) {
+		DOMSource xmlSource = toDOMSource(xmlString);
+		return fromXml(xmlSource, c);
+	}
+
+	public static <T> T fromXml2(String xmlString, Class<T> c) {
+		Reader reader = new StringReader(xmlString);
+		return fromXml(reader, c);
+	}
+
+	public String asJson() {
 		String jsonString = null;
 		ObjectMapper mapper = getMapper();
-		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-		mapper.setSerializationInclusion(Include.NON_EMPTY);
 		try {
 			jsonString = mapper.writeValueAsString(this);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			LOGGER.log(Level.SEVERE, "Jackson could not convert the object correctly.", JsonProcessingException.class);
-			throw new RuntimeException("Jackson could not convert the object correctly.");	
+			throw new RuntimeException("Jackson could not convert the object correctly.");
 		}
 		return jsonString;
 	}
 
-	public static <T> String asJson(List<T> list){
-	
+	public static <T> String asJson(List<T> list) {
 		String jsonString = null;
 		ObjectMapper mapper = getMapper();
-		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-		mapper.setSerializationInclusion(Include.NON_EMPTY); 	// equivale a @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
-		// mapper.setSerializationInclusion(Include.NON_NULL); 	// equivale a @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-		// If you need both non-null and non-empty, use @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT) 
 		try {
 			jsonString = mapper.writeValueAsString(list);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			LOGGER.log(Level.SEVERE, "Jackson could not convert the object correctly.", JsonProcessingException.class);
-			throw new RuntimeException("Jackson could not convert the object correctly.");	
+			throw new RuntimeException("Jackson could not convert the object correctly.");
 		}
 		return jsonString;
-	}	
-	
-	public String asPrettyJson(){
+	}
+
+	public String asPrettyJson() {
 		String jsonPrettyString = null;
 		ObjectMapper mapper = getMapper();
 		try {
-			jsonPrettyString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return jsonPrettyString;
-	}		
-	
-	private String asJsonFiltered(){
-		String jsonPrettyString = null;
-		ObjectMapper mapper = getMapper();
-		//By default all fields without explicit view definition are included, disable this
-		mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);	
-//	    mapper.configure(MapperFeature.AUTO_DETECT_FIELDS, false);
-//	    mapper.configure(MapperFeature.AUTO_DETECT_IS_GETTERS, false);
-//	    mapper.configure(MapperFeature.AUTO_DETECT_GETTERS, false);
-//	    mapper.configure(MapperFeature.AUTO_DETECT_SETTERS, false);
-//	    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-//	    mapper.configure(SerializationFeature.INDENT_OUTPUT, true);			
-		try {
-			// String jsonString = mapper.writeValueAsString(this);
 			jsonPrettyString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 		return jsonPrettyString;
 	}
-	
+
+	private String asJsonFiltered() {
+		String jsonPrettyString = null;
+		ObjectMapper mapper = getMapper();
+		try {
+			jsonPrettyString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return jsonPrettyString;
+	}
+
 	public static <T> T fromJson(String json, Class<T> c) {
 		T t = null;
-		ObjectMapper mapper = getMapper();		
+		ObjectMapper mapper = getMapper();
 		try {
 			t = mapper.readValue(json, c);
 		} catch (IOException e) {
@@ -170,44 +155,72 @@ public class RootModel {
 		}
 		return t;
 	}
-	
+
 	public static ObjectMapper getMapper() {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setDateFormat(FORMAT1);
-		JaxbAnnotationIntrospector d;
-        // Add support for both Jaxb and Jackson annotations		
-		 AnnotationIntrospector aiJackson = new JacksonAnnotationIntrospector();
-		 AnnotationIntrospector aiJaxb = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
-		 // first Jaxb, second Jackson annotations
-	     mapper.setAnnotationIntrospector(AnnotationIntrospector.pair(aiJaxb, aiJackson));
 
-		// Il seguente statement equivale all'annotazione (di classe) @JsonIgnoreProperties(ignoreUnknown = true)
+		// By default all fields without explicit view definition are included.
+		// If enabled, non-annotated properties will be included;
+		// when disabled, they will be excluded.
+		// Feature that determines whether properties that have no view annotations are included in JSON serialization views 
+		mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+
+		// Feature that determines what happens when no accessors are found for a type
+		// (and there are no annotations to indicate it is meant to be serialized).
+		// If enabled (default), an exception is thrown to indicate these as
+		// non-serializable types;
+		// if disabled, they are serialized as empty Objects, i.e. without any properties.
+		// Note that empty types that this feature has only effect on those "empty"
+		// beans that do not have any recognized annotations (like @JsonSerialize): ones
+		// that do have annotations do not result in an exception being thrown.
+		// Feature is enabled by default.
+		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+		// SerializationInclusion: ovvero gestione di null, stringhe vuote e collection
+		// vuote !!!
+		mapper.setSerializationInclusion(Include.ALWAYS); 	// Includo null e stringhe vuote // Value that indicates that
+															// property is to be always included, independent of value
+															// of the property.
+		// mapper.setSerializationInclusion(Include.NON_EMPTY);
+		// equivale a @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+		// mapper.setSerializationInclusion(Include.NON_NULL);
+		// equivale a @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+		// If you need both non-null and non-empty, use
+		// mapper.setSerializationInclusion(Include.NON_DEFAULT) or
+		// @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
+
+		// Add support for both Jaxb and Jackson annotations
+		AnnotationIntrospector aiJackson = new JacksonAnnotationIntrospector();
+		AnnotationIntrospector aiJaxb = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+		// First Jaxb, second Jackson annotations
+		mapper.setAnnotationIntrospector(AnnotationIntrospector.pair(aiJaxb, aiJackson));
+
+		// Il seguente statement equivale all'annotazione (di classe)
+		// @JsonIgnoreProperties(ignoreUnknown = true)
+		// Feature that determines whether encountering of unknown properties (ones that
+		// do not map to a property, and there is no "any setter" or handler that can
+		// handle it) should result in a failure (by throwing a JsonMappingException) or
+		// not.
 		// mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		return mapper;
 	}
 
-	public static StreamSource toStreamSource(String str){
+	public static StreamSource toStreamSource(String str) {
 		StreamSource source = new StreamSource(new StringReader(str));
 		return source;
 	}
-	
-	public static DOMSource toDOMSource(String str){
+
+	public static DOMSource toDOMSource(String str) {
 		DOMSource source = null;
 		Element node = null;
 		try {
-			// Java 7
-			//ByteArrayInputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
-			// Java 6
-			ByteArrayInputStream is = new ByteArrayInputStream(str.getBytes("UTF-8"));	
+			ByteArrayInputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
 			// oppure
 			// InputSource is = new InputSource();
 			// is.setCharacterStream(new StringReader(str));
 			// is.setEncoding("UTF-8");
-			node = DocumentBuilderFactory
-				    .newInstance()
-				    .newDocumentBuilder()
-				    .parse(is)
-				    .getDocumentElement();
+			node = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is).getDocumentElement();
 		} catch (SAXException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -221,22 +234,22 @@ public class RootModel {
 
 	public static String toString(Date date) {
 		String formatted = null;
-		if(date!=null){				
+		if (date != null) {
 			formatted = FORMAT1.format(date);
 		}
-		return formatted; 
-	}	
-	
+		return formatted;
+	}
+
 	public static String toString(GregorianCalendar cal) {
-		if(cal!=null){				
+		if (cal != null) {
 			return toString(cal.getTime());
 		}
 		return null;
 	}
-	
+
 	public static Date toDate(String str) {
 		Date d = null;
-		if(str!=null){					
+		if (str != null) {
 			try {
 				d = FORMAT1.parse(str);
 			} catch (ParseException e) {
@@ -248,27 +261,26 @@ public class RootModel {
 
 	protected static Date toDate(GregorianCalendar cal) {
 		Date d = null;
-		if(cal!=null){
+		if (cal != null) {
 			d = cal.getTime();
 		}
 		return d;
 	}
-	
+
 	protected static GregorianCalendar toCal(Date d) {
 		GregorianCalendar cal = new GregorianCalendar();
-		if(cal!=null){
+		if (cal != null) {
 			cal.setTime(d);
 		}
 		return cal;
-	}	
-	
-	protected static String toString(BigDecimal bd){
+	}
+
+	protected static String toString(BigDecimal bd) {
 		String str = null;
-		if(bd!=null){
+		if (bd != null) {
 			str = bd.toString();
 		}
 		return str;
 	}
-		
-	
+
 }
