394827794622c3874e13fa381eaff17e72208e35
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2018-04-20 15:18:18 +0200

    Committed by Robo (from ANDREA-WIN10 at 2018-04-20 15:18) on 2018-04-20 15:18

---
+++ src/test/java/it/iubar/desktop/api/HttpClientTest.java
@@ -0,0 +1,14 @@
+package it.iubar.desktop.api;
+
+public class HttpClientTest {
+
+	
+ 	private HttpClient client = null; 	
+ 	// PresenzeImport usa client HttpClient
+ 	
+// 	-- iubar paghe logic
+// 	private DocClient client2 = null;
+// 	private RestClientC client3 = null;
+// 	private RestClientP client4 = null;
+	
+}
