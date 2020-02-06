3d845d9f32f1bd9af73bbbfb8fee9398e9602605
borgogelli <borgogelli@iubar.it>
2020-02-06 19:01:56 +0100

    Committed by Robo (from ANDREA-WIN10)

--- src/main/java/it/iubar/desktop/api/ClientStatus.java
+++ src/main/java/it/iubar/desktop/api/ClientStatus.java
@@ -3,27 +3,13 @@ package it.iubar.desktop.api;
 public class ClientStatus {
 
     public static AppStatus app_status = AppStatus.UNKNWON;
-	public static AppStatus2 app_status2 = AppStatus2.UNKNWON;
 	
 	public static String getString() {
-		String s1 = ClientStatus.app_status.toString();
-
-		String status = s1;
-		if(ClientStatus.app_status2 != AppStatus2.UNKNWON) {
-			String s2 = ClientStatus.app_status2.toString();
-			s1 = s1 + " / " + s2;
-		}
+		String status = ClientStatus.app_status.toString();
 		if(ClientStatus.app_status == AppStatus.UNKNWON) {
 			status = "<sconosciuto>";
 		}
 		return status;
 	}
 
-	public static boolean isAppRegistered() {
-		if(ClientStatus.app_status2 == AppStatus2.OK) {
-			return true;
-		}
-		return false;
-	}
-	
 }
