afde4d7c2ab5be3b0f549ed23349ee5b8a160f61
borgogelli <borgogelli@iubar.it>
2020-07-17 15:49:32 +0200

    .

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -6,5 +6,10 @@ public class RestApiConsts {
  
 	public static final String PRESENZE_BASE_ROUTE = "https://presenze.iubar.it/api/v1/";
 	public static final String CEDOLINI_BASE_ROUTE = "https://cedolini.iubar.it/api/v1/";
+	public static final String IUBAR_HR_OAUTH_CLIENT_SECRECT = "";
+	public static final String IUBAR_HR_OAUTH_CLIENT_ID = "";
+	public static final String IUBAR_HR_OAUTH_LOGIN_URL = "";
+	public static final String IUBAR_HR_BASE_URL = "";
+	public static final String IUBAR_HR_BASE_API_URL = "";
 	
 }
