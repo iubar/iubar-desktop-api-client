13c1d9f3f4ee271d5e507d641a94492f563d8e11
Daniele <daniele.montesi@gmail.com>
2020-07-20 08:19:28 +0200

    .

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -3,8 +3,8 @@ package it.iubar.desktop.api;
 public class RestApiConsts {
 
 	public static final String CRM_BASE_ROUTE = "https://iubar.it/crm/api/crm/v1";
- 
-	public static final String PRESENZE_BASE_ROUTE = "https://presenze.iubar.it/api/v1/";
-	public static final String CEDOLINI_BASE_ROUTE = "https://cedolini.iubar.it/api/v1/";
 	
+	public static final String IUBAR_HR_BASE_URL = "https://hr.iubar.it";
+	public static final String IUBAR_HR_BASE_API_URL = IUBAR_HR_BASE_URL + "/api/v1";	
+	public static final String IUBAR_HR_OAUTH_LOGIN_URL = IUBAR_HR_BASE_URL + "/oauth/token";
 }
