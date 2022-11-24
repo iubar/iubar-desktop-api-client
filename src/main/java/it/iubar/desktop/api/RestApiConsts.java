233ec710a03b61018cd5c9ba4f18489fcfaf85f4
Daniele <daniele.montesi@gmail.com>
2022-11-24 17:30:43 +0100

    Committed by Robo

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -5,7 +5,7 @@ public class RestApiConsts {
 	public static final String CRM_BASE_ROUTE = "https://iubar.it/crm/api/crm/v1";
 	
 	public static final String IUBAR_HR_BASE_URL = "https://hr.iubar.it";
-	//public static final String IUBAR_HR_BASE_URL = "http://hr.iubar.local";
+//	public static final String IUBAR_HR_BASE_URL = "http://hr.iubar.local";
 	public static final String IUBAR_HR_BASE_API_URL = IUBAR_HR_BASE_URL + "/api/v1";	
 	public static final String IUBAR_HR_OAUTH_LOGIN_URL = IUBAR_HR_BASE_URL + "/oauth/token";
 }
