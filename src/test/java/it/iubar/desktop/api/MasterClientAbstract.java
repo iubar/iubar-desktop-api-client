c376970a482522a9b0876985cff72961e1141fa7
Daniele <daniele.montesi@gmail.com>
2023-11-17 11:16:36 +0100

    Committed by Robo

--- src/test/java/it/iubar/desktop/api/MasterClientAbstract.java
+++ src/test/java/it/iubar/desktop/api/MasterClientAbstract.java
@@ -1,6 +1,6 @@
 package it.iubar.desktop.api;
 
- 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -22,81 +22,66 @@ import it.iubar.desktop.api.models.ModelsList;
 import it.iubar.desktop.api.models.TitolareModel;
 import it.iubar.desktop.api.models.TitolareModelTest;
 import jakarta.json.JsonObject;
- 
+
 public abstract class MasterClientAbstract {
 
 	private static final Logger LOGGER = Logger.getLogger(MasterClientAbstract.class.getName());
-	
-    private static final String MAC_1 = "B8-CA-3A-96-BD-03";
-    private static final String MAC_2 = MAC_1;
- 
+
+	private static final String MAC_1 = "B8-CA-3A-96-BD-03";
+	private static final String MAC_2 = MAC_1;
+
 
 	private static final String APP_FAMILY_PAGHE = "paghe";
 	public static final int ID_APP_PAGHEOPEN = 11;
 	public static final int ID_FAMILY_PAGHE = 1;
 	public static String user = null;
 	public static String apiKey = null;
-	  
+
 	protected static ClientModel client = null;
-    private static ModelsList<TitolareModel> titolari = null;
+	private static ModelsList<TitolareModel> titolari = null;
 
-    public static void loadConfig() {
-    	String jwtUser = System.getenv("JWT_USER");
-    	String jwtApiKey = System.getenv("JWT_APIKEY");
-    	if(jwtUser!=null && jwtUser.length()>0) {
-    		MasterClientAbstract.user = jwtUser;
-    		MasterClientAbstract.apiKey = jwtApiKey;
-    	}else {
-    		String config = "/secret.properties";    		
-    		Properties prop = new Properties();
-    		InputStream is = null;
-    		
-    		try {
-    			is = MasterJwtClientTest.class.getResourceAsStream(config);
-    			prop.load(is);
-    			MasterClientAbstract.user = prop.getProperty("JWT_USER");
-    			MasterClientAbstract.apiKey = prop.getProperty("JWT_APIKEY");
-    		} catch (IOException ex) {
-    			LOGGER.log(Level.WARNING, "Impossibile leggere la configurazione dal file " + config);
-    			LOGGER.log(Level.WARNING, ex.getMessage());
-    			// ex.printStackTrace();
-    		} finally {
-    			if (is != null) {
-    				try {
-    					is.close();
-    				} catch (IOException e) {
-    					e.printStackTrace();
-    				}
-    			}
-    		}
-    	}
-    	
-    	if(MasterClientAbstract.user == null || MasterClientAbstract.user.length()==0) { 
-    		LOGGER.log(Level.SEVERE,"Impossibile continuare: user non specificato");
-    		System.exit(1);
-    	}else if(MasterClientAbstract.apiKey == null || MasterClientAbstract.apiKey.length()==0) {
-    		LOGGER.log(Level.SEVERE, "Impossibile continuare: apiKey non specificata");
-    		System.exit(1);
-    	}
-    	
-    }
-    
-    @BeforeAll
-    public static void initData() throws IOException {
-    	int idApp = MasterClientAbstract.ID_APP_PAGHEOPEN;
-    	MasterClientAbstract.client = ClientModelTest.factory();
-        
-        MasterClientAbstract.titolari= new ModelsList<TitolareModel>(MAC_1, idApp, "titolari");
-        TitolareModel titolare = TitolareModelTest.factory();
-        MasterClientAbstract.titolari.add(titolare);
-    }
+	public static void loadConfig() throws IOException {
+		String jwtUser = System.getenv("JWT_USER");
+		String jwtApiKey = System.getenv("JWT_APIKEY");
+		if(jwtUser!=null && jwtUser.length()>0) {
+			MasterClientAbstract.user = jwtUser;
+			MasterClientAbstract.apiKey = jwtApiKey;
+		}else {
+			String config = "/secret.properties";    		
+			Properties prop = new Properties();
+			InputStream is = MasterJwtClientTest.class.getResourceAsStream(config);
+			prop.load(is);
+			MasterClientAbstract.user = prop.getProperty("JWT_USER");
+			MasterClientAbstract.apiKey = prop.getProperty("JWT_APIKEY");
+			is.close();
+		}
+
+		if(MasterClientAbstract.user == null || MasterClientAbstract.user.length()==0) { 
+			LOGGER.log(Level.SEVERE,"Impossibile continuare: user non specificato");
+			System.exit(1);
+		}else if(MasterClientAbstract.apiKey == null || MasterClientAbstract.apiKey.length()==0) {
+			LOGGER.log(Level.SEVERE, "Impossibile continuare: apiKey non specificata");
+			System.exit(1);
+		}
+
+	}
+
+	@BeforeAll
+	public static void initData() throws IOException {
+		int idApp = MasterClientAbstract.ID_APP_PAGHEOPEN;
+		MasterClientAbstract.client = ClientModelTest.factory();
+
+		MasterClientAbstract.titolari= new ModelsList<TitolareModel>(MAC_1, idApp, "titolari");
+		TitolareModel titolare = TitolareModelTest.factory();
+		MasterClientAbstract.titolari.add(titolare);
+	}
 
 
-    abstract protected IHttpClient clientFactory();
-    
+	abstract protected IHttpClient clientFactory();
+
 	private boolean checkMacBlacklist(String mac) throws Exception{
 		boolean b = false;
-    	IHttpClient masterClient = clientFactory();
+		IHttpClient masterClient = clientFactory();
 
 		JsonObject jsonObject = masterClient.responseManager(masterClient.get("/public/" + APP_FAMILY_PAGHE + "/blacklist/mac/" + mac));
 		b = jsonObject.getBoolean("data");
@@ -108,71 +93,60 @@ public abstract class MasterClientAbstract {
 
 		return b;
 	}
-	
-    @Test
-    public void sendTest() {
-    	IHttpClient masterClient = clientFactory();
-        try {
-        	
-        	// Test on Production 
-        	
-        	  JsonObject jsonObj1 = masterClient.send(MasterClientAbstract.client);
-            assertNotNull(jsonObj1);
-            assertTrue(jsonObj1.getInt("data")>=0);
-              JsonObject jsonObj2 = masterClient.send(MasterClientAbstract.titolari);
-            assertNotNull(jsonObj2);
-            assertTrue(jsonObj2.getInt("data")>=0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-		}
-    }
-    
-//    @Ignore("Ignoring test until the service is online")
-//    @Test
-//    public void checkMacTest() throws Exception {
-//        IMasterClient masterClient = new MasterClient("src/main/resources/config.ini");
-//        ListMac listMac = masterClient.checkMac("1C-4B-D6-C8-8B-31");
-//        assertEquals(true,listMac.isGreyList());
-//        assertEquals(2,listMac.getCodeGreyList());
-//        assertEquals("Non ha pagato", listMac.getDescGreyList());
-//        assertEquals(false, listMac.isBlackList());
-//        listMac = masterClient.checkMac("84-2B-2B-91-78-96");
-//        assertEquals(false, listMac.isGreyList());
-//        assertEquals(0, listMac.getCodeGreyList());
-//        assertEquals("", listMac.getDescGreyList());
-//        assertEquals(true, listMac.isBlackList());
-//    }
 
-       @Test
-       public void sendTestOnEchoServer(){
-           IHttpClient masterClient = clientFactory();
-           try {
-           	// Test on Echo server        
-           	String url1 = HttpClientTest.ECHO_API;
-           	  JsonObject jsonObj = masterClient.send(url1, MasterClientAbstract.client);
-           	assertNotNull(jsonObj);
-           } catch (Exception e) {
-           	e.printStackTrace();
-           	fail();
-           }
-       }
-       @Test
-   	public void testMacBlacklist_1() throws Exception{ 	
-       	boolean b = checkMacBlacklist(MAC_1);
-       	assertFalse(b);
-       }
-       
-       @Test
-   	public void testMacBlacklist_2() throws Exception{ 	
-       	boolean b = checkMacBlacklist(MAC_2);
-       	assertFalse(b);
-       }    
-       
-       
- 
-    @AfterAll
-    public static void delFiles(){
-   
-    }
+	@Test
+	public void sendTest() throws Exception {
+		IHttpClient masterClient = clientFactory();        	
+		// Test on Production 
+
+		JsonObject jsonObj1 = masterClient.send(MasterClientAbstract.client);
+		assertNotNull(jsonObj1);
+		assertTrue(jsonObj1.getInt("data")>=0);
+		JsonObject jsonObj2 = masterClient.send(MasterClientAbstract.titolari);
+		assertNotNull(jsonObj2);
+		assertTrue(jsonObj2.getInt("data")>=0);
+	}
+
+	//    @Ignore("Ignoring test until the service is online")
+	//    @Test
+	//    public void checkMacTest() throws Exception {
+	//        IMasterClient masterClient = new MasterClient("src/main/resources/config.ini");
+	//        ListMac listMac = masterClient.checkMac("1C-4B-D6-C8-8B-31");
+	//        assertEquals(true,listMac.isGreyList());
+	//        assertEquals(2,listMac.getCodeGreyList());
+	//        assertEquals("Non ha pagato", listMac.getDescGreyList());
+	//        assertEquals(false, listMac.isBlackList());
+	//        listMac = masterClient.checkMac("84-2B-2B-91-78-96");
+	//        assertEquals(false, listMac.isGreyList());
+	//        assertEquals(0, listMac.getCodeGreyList());
+	//        assertEquals("", listMac.getDescGreyList());
+	//        assertEquals(true, listMac.isBlackList());
+	//    }
+
+	@Test
+	public void sendTestOnEchoServer() throws Exception{
+		IHttpClient masterClient = clientFactory();
+		// Test on Echo server        
+		String url1 = HttpClientTest.ECHO_API;
+		JsonObject jsonObj = masterClient.send(url1, MasterClientAbstract.client);
+		assertNotNull(jsonObj);
+	}
+	@Test
+	public void testMacBlacklist_1() throws Exception{ 	
+		boolean b = checkMacBlacklist(MAC_1);
+		assertFalse(b);
+	}
+
+	@Test
+	public void testMacBlacklist_2() throws Exception{ 	
+		boolean b = checkMacBlacklist(MAC_2);
+		assertFalse(b);
+	}    
+
+
+
+	@AfterAll
+	public static void delFiles(){
+
+	}
 }
