fbc52f53c29c3dd462de766f7084d853d2a6532f
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2025-07-09 18:46:14 +0200

    .

--- src/main/java/it/iubar/desktop/api/HttpClient.java
+++ src/main/java/it/iubar/desktop/api/HttpClient.java
@@ -3,6 +3,7 @@ package it.iubar.desktop.api;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import javax.net.ssl.SSLContext;
@@ -18,59 +19,57 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.Response;
 
- 
-
-
 public abstract class HttpClient {
 
 	private static final Logger LOGGER = Logger.getLogger(HttpClient.class.getName());
-	private static final int DEF_CONNECT_TIMEOUT = 15000;
-	private static final int DEF_READ_TIMEOUT = 15000;
+	private static final int DEF_CONNECT_TIMEOUT = 10000;
+	private static final int DEF_READ_TIMEOUT = 10000;
 	protected String url = null;
-		
+
 	public static Client newClient() {
 		Client client = ClientBuilder.newClient();
 		client.property(ClientProperties.CONNECT_TIMEOUT, DEF_CONNECT_TIMEOUT);
-		client.property(ClientProperties.READ_TIMEOUT, DEF_READ_TIMEOUT);		
+		client.property(ClientProperties.READ_TIMEOUT, DEF_READ_TIMEOUT);
 		return client;
 	}
- 	
-	
-	public static Client newClient2025(){
-		 Client client = null;
-		 
-        // Crea un SSLContext con la versione desiderata (es. TLSv1.3 o TLSv1.2)
-        SSLContext sslContext = null; 
+
+	public static Client newClient2025() {
+		Client client = null;
+
+		// Crea un SSLContext con la versione desiderata (es. TLSv1.3 o TLSv1.2)
+		SSLContext sslContext = null;
 		try {
 			sslContext = SSLContext.getInstance("TLSv1.3");
-	        sslContext.init(null, null, null); // Usa truststore/keystore di default
+			sslContext.init(null, null, null); // Usa truststore/keystore di default
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (KeyManagementException e) {
 			e.printStackTrace();
 		}
 
-if(sslContext!=null) {
-        // Costruisci il client JAX-RS con il SSLContext custom
-          client = ClientBuilder.newBuilder()
-        		// .property("jersey.config.client.followRedirects", true)
-                .sslContext(sslContext)
-                .build();
+		if (sslContext != null) {
+			// Costruisci il client JAX-RS con il SSLContext custom
+			client = ClientBuilder.newBuilder()
+					// .property("jersey.config.client.followRedirects", true)
+					.sslContext(sslContext)
+					.connectTimeout(5, TimeUnit.SECONDS) // Timeout di connessione
+					.readTimeout(10, TimeUnit.SECONDS) // Timeout di lettura
+					.build();
 
-        // Opzionalmente forza le versioni TLS supportate
-        SSLParameters sslParams = sslContext.getDefaultSSLParameters();
-        sslParams.setProtocols(new String[]{"TLSv1.3"});
+			// Opzionalmente forza le versioni TLS supportate
+			SSLParameters sslParams = sslContext.getDefaultSSLParameters();
+			sslParams.setProtocols(new String[] { "TLSv1.3" });
 
- 
-}
-        return client;
+		}
+		return client;
 	}
-	
+
 	/**
-	 * Crea il client con autenticazione user/password, e ignora la validità del certificato SSL
+	 * Crea il client con autenticazione user/password, e ignora la validità del
+	 * certificato SSL
 	 * 
 	 */
-	public static Client newClientProtected(String user, String password)  {
+	public static Client newClientProtected(String user, String password) {
 		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
 			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
 				return null;
@@ -81,7 +80,7 @@ if(sslContext!=null) {
 
 			public void checkServerTrusted(X509Certificate[] certs, String authType) {
 			}
-		} 
+		}
 		};
 
 		SSLContext sslContext = null;
@@ -95,29 +94,24 @@ if(sslContext!=null) {
 			LOGGER.severe("ERRORE: " + e.getMessage());
 		}
 
-        Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
-        client.property(ClientProperties.CONNECT_TIMEOUT, 2000)
-        .register(HttpAuthenticationFeature.basic(user, password));
-		
+		Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
+		client.property(ClientProperties.CONNECT_TIMEOUT, 2000)
+				.register(HttpAuthenticationFeature.basic(user, password));
+
 		return client;
 	}
 
-	
-
-
 	public static JsonObject getAnswer(Response response) {
 		JsonObject answer = null;
-		if(response!=null) {
-		String output = response.readEntity(String.class);
- 		try {
-			answer = it.iubar.desktop.api.json.JsonUtils.parseJsonString(output);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		if (response != null) {
+			String output = response.readEntity(String.class);
+			try {
+				answer = it.iubar.desktop.api.json.JsonUtils.parseJsonString(output);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return answer;
 	}
 
-	
- 
 }
