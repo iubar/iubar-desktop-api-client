91c6e72e505a6f36b53a35747c19b878b7ad9fd6
andreas <andrea.serafini@gmail.com>
2026-04-29 12:47:43 +0200

    .

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -4,7 +4,7 @@ public class RestApiConsts {
 
 	public static final String CRM_BASE_ROUTE = "https://common.iubar.it/crm/v1";
 	
-	public static final String IUBAR_HR_BASE_URL = "https://hr.iubar.it";
+	public static final String IUBAR_HR_BASE_URL = "https://hr2.iubar.it";
 //	public static final String IUBAR_HR_BASE_URL = "http://127.0.0.1:8000";
 	public static final String IUBAR_HR_BASE_API_URL = IUBAR_HR_BASE_URL + "/api/v1";	
 	public static final String IUBAR_HR_OAUTH_LOGIN_URL = IUBAR_HR_BASE_URL + "/oauth/token";
