bbc76e19c9913c1258ac78eec18db4a6472c3fd9
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2026-07-10 15:54:30 +0200

    Committed by Robo

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -4,7 +4,7 @@ public class RestApiConsts {
 
 	public static final String CRM_BASE_ROUTE = "https://common.iubar.it/crm/v1";
  
-	public static final String IUBAR_HR_BASE_URL = "https://hr2.iubar.it";
+	public static final String IUBAR_HR_BASE_URL = "https://hr.iubar.it";
 	
 	public static final String PROD_URL = IUBAR_HR_BASE_URL;
 	public static final String DEV_URL = "http://192.168.0.131:3000";
