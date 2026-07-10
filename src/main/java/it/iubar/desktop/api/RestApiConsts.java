f10b156ff4e2d4f8757fa2f6c71a09ab7360729c
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2026-07-10 16:07:11 +0200

    .

--- src/main/java/it/iubar/desktop/api/RestApiConsts.java
+++ src/main/java/it/iubar/desktop/api/RestApiConsts.java
@@ -7,7 +7,7 @@ public class RestApiConsts {
 	public static final String IUBAR_HR_BASE_URL = "https://hr.iubar.it";
 	
 	public static final String PROD_URL = IUBAR_HR_BASE_URL;
-	public static final String DEV_URL = "http://192.168.0.131:3000";
+	public static final String DEV_URL = "http://192.168.0.131:3013";
 	public static final String STAGE_URL = "http://192.168.0.107:3013";
 	public static final String LOCAL_URL = "http://localhost:3000";
 	
